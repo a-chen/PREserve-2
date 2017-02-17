3a1aec24183325abc7a14cdc8ddefe4caf79eae1
Andrew Chen <achen.this@gmail.com>
2017-02-17 16:23:40 -0500

    Initial commit

---
+++ preserveClient/src/main/webapp/app/app.module.js
@@ -0,0 +1,1 @@
+// The app.module.js file will handle the setup of your app, load in AngularJS dependencies and so on
---
+++ preserveClient/src/main/webapp/app/app.routes.js
@@ -0,0 +1,1 @@
+// The app.route.js file will handle all the routes and the route configuration
