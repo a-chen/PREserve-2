c0cbd7d14ad101bdfeb7679a0575872f084f8027
Andrew Chen <achen.this@gmail.com>
2017-02-18 15:56:45 -0500

    Moved REST calls into api factory

--- PREserve/src/main/resources/static/app/app.module.js
+++ PREserve/src/main/resources/static/app/app.module.js
@@ -1,42 +1,24 @@
 // The app.module.js file will handle the setup of your app, load in AngularJS dependencies and so on
 
-// load modules into main module
-angular.module('app', ['delegate', 'api']);
+/*// load modules into main module
+ angular.module('app', ['delegate', 'api']);
 
-// declare additional modules
-angular.module('delegate', []);
-angular.module('api', []);
+ // declare additional modules
+ angular.module('delegate', []);
+ angular.module('api', []);*/
 
 // customer controller
-angular.module('app').controller(
-    'customerController', function customerController($scope, $http, $log) {
+angular.module('app', ['api'])
+    .controller('CustomerController',
+        function CustomerController($scope, $log, apiFactory) {
 
-    // $scope.customers = delegateFactory.getAllCustomers();
+            $log.debug('Starting CustomerController');
 
-        $http({
-            url: 'http://localhost:9001/customerService/customer/',
-            method: 'GET'
-        })
-            .then(
-                function (successResponse) {
-                    $log.debug('Retrieved all customers: ');
-                    $log.debug(successResponse.data);
-                    $scope.customers = successResponse.data;
-                },
-                function (errorResponse) {
-                    $log.error('Failed to retrieve all customers: ' + errorResponse.status);
-                }
-            );
-    /*$scope.customers = [
-        {
-            firstName: 'John',
-            lastName: 'Wick'
-        }, {
-            firstName: 'Jason',
-            lastName: 'Bourne'
-        }, {
-            firstName: 'James',
-            lastName: 'Bond'
-        }
-    ];*/
-});
+            // makes call, then calls .then() to manipulate data after promise object is returned
+            apiFactory.getAllCustomers()
+                .then(function (data) {
+                    $log.debug('Promised data');
+                    $log.debug(data);
+                    $scope.customers = data;
+                });
+        });
--- PREserve/src/main/resources/static/app/components/api.js
+++ PREserve/src/main/resources/static/app/components/api.js
@@ -1,24 +1,46 @@
 /**
  * API for making AJAX calls to PREserve endpoints
  */
-angular.module('api').factory('apiFactory', function ($log, $http) {
-    $log.debug('Started api');
 
-    var api = {};
+angular.module('api', [])
+    .factory('apiFactory', function($http, $log) {
 
-    api.getAllCustomers = function () {
-        return $http({
-            url: 'http://localhost:9001/customer',
-            method: 'GET'
-        })
-            .then(
-                function (successResponse) {
-                    $log.debug('Retrieved all customers: ' + successResponse);
-                    return successResponse.data;
-                },
-                function (errorResponse) {
-                    $log.error('Failed to retrieve all customers: ' + errorResponse.status);
+        $log.debug('Starting api');
+
+        // create object to be returned
+        var api = {};
+
+       /* var sampleCustomers = [
+                {
+                    firstName: 'John',
+                    lastName: 'Wick'
+                }, {
+                    firstName: 'Jason',
+                    lastName: 'Bourne'
+                }, {
+                    firstName: 'James',
+                    lastName: 'Bond'
                 }
-            );
-    }
-});
+            ]; */
+
+        api.getAllCustomers = function () {
+            return $http({
+                url: 'http://localhost:9001/customerService/customer',
+                method: 'GET'
+            })
+                .then(
+                    function (successResponse) {
+                        $log.debug('Retrieved all customers with code: ' + successResponse.status);
+                        $log.debug(successResponse.data);
+                        return successResponse.data;
+                    },
+                    function (errorResponse) {
+                        $log.error('Failed to retrieve all customers with code: ' + errorResponse.status);
+                        $log.error(errorResponse);
+                    }
+                );
+        };
+
+       // return object to call methods on
+       return api;
+    });
